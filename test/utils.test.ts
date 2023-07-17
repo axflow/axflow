52b603cc69079da789befd41bdcad6799da0805a
Ben Reinhart <benjreinhart@gmail.com>
2023-07-16 22:20:05 -0700

    Add template formatter for prompts (#12)
    Will want this formatting for more robust prompt functionality.

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,4 +1,4 @@
-import { zip } from '../src/utils';
+import { formatTemplate, zip } from '../src/utils';
 
 describe('utils', () => {
   describe('zip', () => {
@@ -17,6 +17,45 @@ describe('utils', () => {
       const l2 = ['2', '4'];
 
       expect(() => zip(l1, l2)).toThrowError('Cannot zip two lists of unequal length');
+    });
+  });
+
+  describe('formatTemplate', () => {
+    const template = `Use the context below to answer the question.
+
+Context:
+{context}
+
+Question:
+{question}
+
+Answer: `;
+
+    it('can format a template', () => {
+      const actual = formatTemplate(template, {
+        context: 'The Golden Gate Bridge opened to the public in 1937.',
+        question: 'When did the Golden Gate Bridge open?',
+      });
+
+      const expected = `Use the context below to answer the question.
+
+Context:
+The Golden Gate Bridge opened to the public in 1937.
+
+Question:
+When did the Golden Gate Bridge open?
+
+Answer: `;
+
+      expect(actual).toEqual(expected);
+    });
+
+    it('throws an error if it cannot find a value for a variable', () => {
+      expect(() =>
+        formatTemplate(template, {
+          context: 'The Golden Gate Bridge opened to the public in 1937.',
+        })
+      ).toThrowError('No value provided for template variable "question"');
     });
   });
 });
