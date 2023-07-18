5a884be429af05f6885a4e2fd66a9f27a78f41c3
Nicholas Charriere <nicholascharriere@gmail.com>
2023-07-18 16:39:29 -0700

    Export the types

--- src/sources/index.ts
+++ src/sources/index.ts
@@ -1,8 +1,10 @@
 import { NAME as fileSystem } from './file-system';
 import { NAME as wikipedia } from './wikipedia';
+import { NAME as textDocument } from './text-document';
 
-export const SUPPORTED_DATA_SOURCES = [fileSystem, wikipedia] as const;
+export const SUPPORTED_DATA_SOURCES = [fileSystem, wikipedia, textDocument] as const;
 export type SupportedDataSources = (typeof SUPPORTED_DATA_SOURCES)[number];
 
 export { FileSystem, type FileSystemOptions } from './file-system';
 export { Wikipedia, type WikipediaOptions } from './wikipedia';
+export { TextDocument, type TextDocumentOptions } from './text-document';
