0ca400793f92f308575f0a028939bdaa27d92f41
Nicholas Charriere <nicholascharriere@gmail.com>
2023-07-18 16:18:44 -0700

    Add text-document data source (#16)
    This will be useful for uploaded files that are already converted to
    utf8 encoded strings.

---
+++ src/sources/text-document.ts
@@ -0,0 +1,25 @@
+import { Document, IDataSource } from '../types';
+
+export const NAME = 'text-document' as const;
+
+export type TextDocumentOptions = {
+  content: string;
+  url: string;
+};
+
+export class TextDocument implements IDataSource {
+  private options: TextDocumentOptions;
+
+  constructor(options: TextDocumentOptions) {
+    this.options = options;
+  }
+
+  async *iterable(): AsyncIterable<Document> {
+    const content = this.options.content;
+
+    yield {
+      url: this.options.url,
+      text: content,
+    };
+  }
+}
