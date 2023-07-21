092fe2d2bdda90d0a5b21be4d04316661d7b9d1a
Ben Reinhart <benjreinhart@gmail.com>
2023-07-20 19:44:24 -0700

    Export classes

--- src/generation/index.ts
+++ src/generation/index.ts
@@ -1,2 +1,4 @@
 export { Completion, type CompletionOptions } from './completion';
+export { ChatCompletion, type ChatCompletionOptions } from './chat-completion';
 export { RAG, type RAGOptions } from './rag';
+export { RAGChat, type RAGChatOptions } from './rag-chat';
