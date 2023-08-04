99ac74ab263381209ee6053442456169b47b9930
Ben Reinhart <benjreinhart@gmail.com>
2023-08-04 13:48:23 -0700

    Remove chroma (#37)
    Too many dependency issues with Chroma and Chroma's peer dependencies
    (which they do not explicitly list as peer dependencies).

--- packages/axgen/src/vector_stores/index.ts
+++ packages/axgen/src/vector_stores/index.ts
@@ -1,8 +1,6 @@
-import { NAME as chroma } from './chroma';
 import { NAME as pinecone } from './pinecone';
 
-export const SUPPORTED_VECTOR_STORES = [chroma, pinecone] as const;
+export const SUPPORTED_VECTOR_STORES = [pinecone] as const;
 export type SupportedVectorStores = (typeof SUPPORTED_VECTOR_STORES)[number];
 
-export { Chroma } from './chroma';
 export { Pinecone } from './pinecone';
