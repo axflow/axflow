4db67d9ffb80da5e5b2d0f1ef56ca0f4a43e1209
Nicholas Charriere <nicholascharriere@gmail.com>
2023-09-04 10:56:55 -0700

    Commit progress

--- packages/models/test/huggingface/generation.test.ts
+++ packages/models/test/huggingface/generation.test.ts
@@ -5,15 +5,15 @@ describe('run', () => {
     try {
       const response = await HfGeneration.run(
         {
-          model: 'gpt2',
+          model: 'google/flan-t5-xxl',
           stream: true,
-          inputs: 'The answer to the universe is',
+          inputs: 'Whats the best way to make a chicken pesto dish?',
           parameters: {
             temperature: 0,
           },
         },
         // TODO mock out the calls later. This is an "integration test" for now
-        { accessToken: process.env.HF_TOKEN! },
+        { accessToken: process.env.HF_TOKEN! }
       );
 
       console.log('response from HF:\n', response);
