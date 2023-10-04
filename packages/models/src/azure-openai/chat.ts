5b5b73c7081149dbd898a858939a0999b30bd6f2
Nicholas Charriere <nicholascharriere@gmail.com>
2023-10-04 16:42:01 -0700

    Add stream(), streamTokens(), streamBytes(), tested locally

--- packages/models/src/azure-openai/chat.ts
+++ packages/models/src/azure-openai/chat.ts
@@ -1,4 +1,5 @@
-import { POST } from '@axflow/models/shared';
+import { POST, HttpError } from '@axflow/models/shared';
+import { streamTransformer } from '../openai/shared';
 
 export function headers(apiKey: string, customHeaders?: Record<string, string>) {
   const headers: Record<string, string> = {
@@ -15,8 +16,8 @@ export function headers(apiKey: string, customHeaders?: Record<string, string>) 
 }
 
 // Our currently supported API version. Swagger spec:
-// https://github.com/Azure/azure-rest-api-specs/blob/main/specification/cognitiveservices/data-plane/AzureOpenAI/inference/stable/2023-05-15/inference.json
-const API_VERSION = '2023-05-15';
+// https://github.com/Azure/azure-rest-api-specs/blob/main/specification/cognitiveservices/data-plane/AzureOpenAI/inference/preview/2023-08-01-preview/inference.json
+const API_VERSION = '2023-08-01-preview';
 /*
  * Create the base URL for the chat/completions endpoint in azure
  * Docs: https://learn.microsoft.com/en-us/azure/ai-services/openai/reference
@@ -109,6 +110,27 @@ export namespace AzureOpenAIChatTypes {
       total_tokens: number;
     };
   };
+
+  export type Chunk = {
+    id: string;
+    object: string;
+    created: number;
+    model: string;
+    choices: Array<{
+      index: number;
+      delta: Delta;
+      finish_reason: 'stop' | 'length' | 'function_call' | null;
+    }>;
+  };
+
+  export type Delta = {
+    role?: 'system' | 'user' | 'assistant' | 'function';
+    content?: string | null;
+    function_call?: {
+      name?: string;
+      arguments?: string;
+    };
+  };
 }
 
 /**
@@ -125,6 +147,7 @@ export namespace AzureOpenAIChatTypes {
  * @param options.fetch A custom implementation of fetch. Defaults to globalThis.fetch.
  * @param options.headers Optionally add additional HTTP headers to the request.
  * @param options.signal An AbortSignal that can be used to abort the fetch request.
+ *
  * @returns an Azure OpenAI chat completion. See Azure's documentation for /chat/completions
  */
 async function run(
@@ -143,6 +166,117 @@ async function run(
   return response.json();
 }
 
+/**
+ * Run a streaming chat completion against the Azure-openAI API. The resulting stream is the raw unmodified bytes from the API.
+ *
+ * @see https://learn.microsoft.com/en-us/azure/ai-services/openai/reference#chat-completions
+ *
+ * @param request The request body sent to Azure. See Azure's documentation for all available parameters.
+ * @param options
+ * @param options.apiKey Azure API key.
+ * @param options.resourceName Azure resource name.
+ * @param options.deploymentId Azure deployment id.
+ * @param options.apiUrl The url of the OpenAI (or compatible) API. If this is passed, resourceName and deploymentId are ignored.
+ * @param options.fetch A custom implementation of fetch. Defaults to globalThis.fetch.
+ * @param options.headers Optionally add additional HTTP headers to the request.
+ * @param options.signal An AbortSignal that can be used to abort the fetch request.
+ *
+ * @returns A stream of bytes directly from the API.
+ */
+async function streamBytes(
+  request: AzureOpenAIChatTypes.Request,
+  options: AzureOpenAIChatTypes.RequestOptions,
+): Promise<ReadableStream<Uint8Array>> {
+  const url = createUrl(options.apiUrl);
+
+  const response = await POST(url, {
+    headers: headers(options.apiKey, options.headers),
+    body: JSON.stringify({ ...request, stream: true }),
+    fetch: options.fetch,
+    signal: options.signal,
+  });
+
+  if (!response.body) {
+    throw new HttpError('Expected response body to be a ReadableStream', response);
+  }
+
+  return response.body;
+}
+
+function noop(chunk: AzureOpenAIChatTypes.Chunk) {
+  return chunk;
+}
+
+/**
+ * Run a streaming chat completion against the Azure-openAI API. The resulting stream is the parsed stream data as JavaScript objects.
+ *
+ * @see https://learn.microsoft.com/en-us/azure/ai-services/openai/reference#chat-completions
+ *
+ * Example object:
+ * {"id":"chatcmpl-864d71dHehdlb2Vjq7WP5nHz10LRO","object":"chat.completion.chunk","created":1696458457,"model":"gpt-4","choices":[{"index":0,"finish_reason":null,"delta":{"content":" me"}}],"usage":null}
+ *
+ * @param request The request body sent to Azure. See Azure's documentation for all available parameters.
+ * @param options
+ * @param options.apiKey Azure API key.
+ * @param options.resourceName Azure resource name.
+ * @param options.deploymentId Azure deployment id.
+ * @param options.apiUrl The url of the OpenAI (or compatible) API. If this is passed, resourceName and deploymentId are ignored.
+ * @param options.fetch A custom implementation of fetch. Defaults to globalThis.fetch.
+ * @param options.headers Optionally add additional HTTP headers to the request.
+ * @param options.signal An AbortSignal that can be used to abort the fetch request.
+ *
+ * @returns A stream of objects representing each chunk from the API.
+ */
+async function stream(
+  request: AzureOpenAIChatTypes.Request,
+  options: AzureOpenAIChatTypes.RequestOptions,
+): Promise<ReadableStream<AzureOpenAIChatTypes.Chunk>> {
+  const byteStream = await streamBytes(request, options);
+  return byteStream.pipeThrough(new OpenAIChatDecoderStream(noop));
+}
+
+/**
+ * Run a streaming chat completion against the Azure-openAI API. The resulting stream emits only the string tokens.
+ *
+ * @see https://learn.microsoft.com/en-us/azure/ai-services/openai/reference#chat-completions
+ *
+ * @param request The request body sent to Azure. See Azure's documentation for all available parameters.
+ * @param options
+ * @param options.apiKey Azure API key.
+ * @param options.resourceName Azure resource name.
+ * @param options.deploymentId Azure deployment id.
+ * @param options.apiUrl The url of the OpenAI (or compatible) API. If this is passed, resourceName and deploymentId are ignored.
+ * @param options.fetch A custom implementation of fetch. Defaults to globalThis.fetch.
+ * @param options.headers Optionally add additional HTTP headers to the request.
+ * @param options.signal An AbortSignal that can be used to abort the fetch request.
+ *
+ * @returns A stream of tokens from the API.
+ */
+async function streamTokens(
+  request: AzureOpenAIChatTypes.Request,
+  options: AzureOpenAIChatTypes.RequestOptions,
+): Promise<ReadableStream<string>> {
+  const byteStream = await streamBytes(request, options);
+  return byteStream.pipeThrough(new OpenAIChatDecoderStream(chunkToToken));
+}
+
+class OpenAIChatDecoderStream<T> extends TransformStream<Uint8Array, T> {
+  constructor(map: (chunk: AzureOpenAIChatTypes.Chunk) => T) {
+    super({ transform: streamTransformer(map) });
+  }
+}
+
+// Note that this function is different than vanilla openAI: we can get an empty array of choices.
+function chunkToToken(chunk: AzureOpenAIChatTypes.Chunk) {
+  if (!chunk.choices || chunk.choices.length === 0) {
+    return '';
+  }
+  return chunk.choices[0].delta.content || '';
+}
+
 export class AzureOpenAIChat {
   static run = run;
+  static streamBytes = streamBytes;
+  static stream = stream;
+  static streamTokens = streamTokens;
 }
