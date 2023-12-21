585be419ad279510e7687c660c8ee346b76c0d8c
Ben Reinhart <benjreinhart@gmail.com>
2023-12-20 16:12:36 -0800

    Add streaming support for Gemini

--- packages/models/test/google/generate-content.test.ts
+++ packages/models/test/google/generate-content.test.ts
@@ -1,21 +1,21 @@
-// import fs from 'node:fs/promises';
-// import Path from 'node:path';
+import fs from 'node:fs/promises';
+import Path from 'node:path';
 
-import { createFakeFetch } from '../utils';
+import { createFakeFetch, createUnpredictableByteStream } from '../utils';
 import { GoogleGenerateContent } from '../../src/google/generate-content';
-// import { StreamToIterable } from '../../src/shared';
+import { StreamToIterable } from '../../src/shared';
 
 describe('google generateContent', () => {
-  // let streamingInferenceResponse: string;
+  let streamingInferenceResponse: string;
 
-  // beforeAll(async () => {
-  //   streamingInferenceResponse = await fs.readFile(
-  //     Path.join(__dirname, 'streaming-inference-response.txt'),
-  //     {
-  //       encoding: 'utf8',
-  //     },
-  //   );
-  // });
+  beforeAll(async () => {
+    streamingInferenceResponse = await fs.readFile(
+      Path.join(__dirname, 'stream-generate-content-response.txt'),
+      {
+        encoding: 'utf8',
+      },
+    );
+  });
 
   describe('run', () => {
     it('executes a chat completion', async () => {
@@ -84,7 +84,128 @@ describe('google generateContent', () => {
           body: expect.any(String),
           method: 'POST',
           headers: {
-            accept: 'application/json',
+            'content-type': 'application/json',
+          },
+        },
+      );
+
+      const args = fetchSpy.mock.lastCall as any;
+      const bodyArgument = JSON.parse(args[1].body);
+
+      expect(bodyArgument).toEqual({
+        contents: [
+          {
+            parts: [
+              {
+                text: 'Write a two sentence story about a magic backpack',
+              },
+            ],
+          },
+        ],
+      });
+    });
+  });
+
+  describe('stream', () => {
+    it('executes a streaming chat completion', async () => {
+      const fetchSpy = createFakeFetch({
+        body: createUnpredictableByteStream(streamingInferenceResponse),
+      });
+
+      const response = await GoogleGenerateContent.stream(
+        {
+          model: 'gemini-pro',
+          contents: [
+            {
+              parts: [
+                {
+                  text: 'Write a two sentence story about a magic backpack',
+                },
+              ],
+            },
+          ],
+        },
+        { apiKey: 'sk-not-real', fetch: fetchSpy as any },
+      );
+
+      let resultingText = '';
+
+      for await (const chunk of StreamToIterable(response)) {
+        resultingText += chunk.candidates[0].content.parts[0].text;
+      }
+
+      expect(resultingText).toEqual(
+        'Emily opened her magical backpack and discovered a world of wonders, from talking animals to flying fairies, all waiting to embark on exciting adventures with her.',
+      );
+
+      expect(fetchSpy).toHaveBeenCalledTimes(1);
+      expect(fetchSpy).toHaveBeenCalledWith(
+        'https://generativelanguage.googleapis.com/v1/models/gemini-pro:streamGenerateContent?key=sk-not-real&alt=sse',
+        {
+          body: expect.any(String),
+          method: 'POST',
+          headers: {
+            'content-type': 'application/json',
+          },
+        },
+      );
+
+      const args = fetchSpy.mock.lastCall as any;
+      const bodyArgument = JSON.parse(args[1].body);
+
+      expect(bodyArgument).toEqual({
+        contents: [
+          {
+            parts: [
+              {
+                text: 'Write a two sentence story about a magic backpack',
+              },
+            ],
+          },
+        ],
+      });
+    });
+  });
+
+  describe('streamTokens', () => {
+    it('streams only the tokens', async () => {
+      const fetchSpy = createFakeFetch({
+        body: createUnpredictableByteStream(streamingInferenceResponse),
+      });
+
+      const response = await GoogleGenerateContent.streamTokens(
+        {
+          model: 'gemini-pro',
+          contents: [
+            {
+              parts: [
+                {
+                  text: 'Write a two sentence story about a magic backpack',
+                },
+              ],
+            },
+          ],
+        },
+        { apiKey: 'sk-not-real', fetch: fetchSpy as any },
+      );
+
+      let resultingText = '';
+
+      for await (const chunk of StreamToIterable(response)) {
+        resultingText += chunk;
+      }
+
+      expect(resultingText).toEqual(
+        'Emily opened her magical backpack and discovered a world of wonders, from talking animals to flying fairies, all waiting to embark on exciting adventures with her.',
+      );
+
+      expect(fetchSpy).toHaveBeenCalledTimes(1);
+      expect(fetchSpy).toHaveBeenCalledWith(
+        'https://generativelanguage.googleapis.com/v1/models/gemini-pro:streamGenerateContent?key=sk-not-real&alt=sse',
+        {
+          body: expect.any(String),
+          method: 'POST',
+          headers: {
             'content-type': 'application/json',
           },
         },
